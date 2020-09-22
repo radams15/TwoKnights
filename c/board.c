953f54e1a3804d80acbd0d3cec4dd831b092a38a
Rhys Adams <email.curly@gmail.com>
2020-09-22 15:06:10 +0100

    Working of C version

---
+++ c/board.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+#define OCCUPIED 1
+#define NONE 0
+
+int** gen_board(){
+    int** out = malloc(8 * sizeof(int*));
+    for(int x=0 ; x<8 ; x++){
+        out[x] = malloc(8*sizeof(int));
+        for(int i=0 ; i<8 ; i++){
+            out[x][i] = NONE;
+        }
+    }
+    return out;
+}
+
+void set_board(int** board, int x, int y, int v){
+    board[x][y] = v;
+}
+
+int get_board(int** board, int x, int y){
+    return board[x][y];
+}
+
+void free_board(int** board){
+    for(int x=0 ; x<8 ; x++){
+        free(board[x]);
+    }
+    free(board);
+}
+
+void print_board(int** board){
+    for(int x=0 ; x<8 ; x++){
+        for(int y=0 ; y<8 ; y++){
+            printf("%d ", board[x][y]);
+        }
+        printf("\n");
+    }
+}
