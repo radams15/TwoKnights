4734b28a02d12163f65eeefddfac45637f3d0b9b
Rhys Adams <radams15@dgsb.co.uk>
2020-09-22 13:05:30 +0100

    Changed Line Endings

--- board.py
+++ board.py
@@ -2,7 +2,7 @@ board = [[0 for _ in range(8)] for _ in range(8)]
 print_board = lambda: [print(x) for x in board]
 
 def get_point(x,y):
-	return board[x][y]
+        return board[x][y]
 
 def set_point(x,y,v):
-	board[x][y] = v
+        board[x][y] = v
