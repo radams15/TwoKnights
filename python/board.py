5d94db7f65fe23caef0c73089bb7674a86c242bb
rhys <email.curly@gmail.com>
2020-09-23 16:37:03 +0100

    Working on another approach

--- python/board.py
+++ python/board.py
@@ -1,13 +1,13 @@
 gen_board = lambda: [[0 for _ in range(8)] for _ in range(8)]
 
 def print_board(board):
-	for x in board:
-		for y in x:
-			print("#" if y is 1 else "_", end="")
-		print()
+    for x in board:
+        for y in x:
+            print("#" if y is 1 else "_", end="")
+        print()
 
-def get_point(board, x,y):
-        return board[x][y]
+def get_board(board, x, y):
+    return board[x][y]
 
-def set_point(board, x,y,v):
-        board[x][y] = v
+def set_board(board, x, y, v):
+    board[x][y] = v
--- python/main.py
+++ python/main.py
@@ -3,64 +3,80 @@ import random
 from board import *
 
 def get_moves(board,x, y):
-        out=[]
-        for x1,y1 in [[1,2],[2,1],[-1,2],[2,-1],[-2,1],[1,-2],[-1,-2],[-2,-1]]:
-                nx,ny = x+x1, y+y1
-                if 0<nx<8 and 0<ny<8:
-                        if get_point(board,nx,ny) is not 1:
-                                out.append((nx,ny))
-        return out
+    out=[]
+    for x1,y1 in [[1,2],[2,1],[-1,2],[2,-1],[-2,1],[1,-2],[-1,-2],[-2,-1]]:
+        nx,ny = x+x1, y+y1
+        if 0<nx<8 and 0<ny<8:
+            if get_board(board, nx, ny) is not 1:
+                out.append((nx,ny))
+    return out
 
 def get_all_moves(board):
-        out = {}
-        for x in range(8):
-                for y in range(8):
-                        moves = get_moves(board, x, y)
-                        if moves:
-                                out[(x,y)] = moves
-        return out
-
-history = [ 0 ]
-i=0
-def run():
-        global i
-        board = gen_board()
-
-        point=(0,0)
-
-        set_point(board,*point, 1)
-        while True:
-                moves = get_moves(board,*point)
-
-                if not moves:
-                        break
-
-                id_to_get = history[-1]
-                point = moves[id_to_get]
-                
-                history.append(point)
-
-                set_point(board,*point, 1)
-                i+=1
-                
-
-        return history
+    out = {}
+    for x in range(8):
+        for y in range(8):
+            moves = get_moves(board, x, y)
+            if moves:
+                out[(x,y)] = moves
+    return out
 
 def print_plots(plots):
-        board = gen_board()
-        for plot in plots:
-               set_point(board, *plot, 1)
-        print_board(board) 
-
-largest=0
-while True:
-        result = run()
+    board = gen_board()
+    for plot in plots:
+        set_board(board, *plot, 1)
+    print_board(board)
 
 
-        length = len(result)
-        largest = max(largest,length)
-        print(largest)
-        if len(result) >= 64:
-                print(result)
-                break
+def run(i, board):
+    point = (0, 0)
 
+    out = []
+
+    moves = get_moves(board, *point)
+
+    if not moves:
+        return out
+
+    print(moves, i)
+    try:
+        point = moves[i]
+    except:
+        return out
+
+    out.append(point)
+
+    set_board(board, *point, 1)
+
+    for move in out:
+        set_board(board, *move, 0)
+
+    return out
+
+def main():
+    board = gen_board()
+
+    point = (0, 0)
+
+    out = []
+
+    set_board(board, *point, 1)
+    while True:
+        moves = get_moves(board, *point)
+
+        if not moves:
+            break
+
+        tried = []
+        for i in range(len(moves)):
+            tried.append([i, run(i, board)])
+
+        id_to_get = sorted(tried, key=lambda move: len(move[1])) # sort by length of try
+        point = moves[id_to_get[0][0]]
+
+        out.append(point)
+
+        set_board(board, *point, 1)
+
+    return out
+
+main()
